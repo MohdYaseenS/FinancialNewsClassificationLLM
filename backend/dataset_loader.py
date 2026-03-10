b2353782d397b1bb6484375759d41eacd81d5421
MohdYaseenS <yaseensajeer2016@gmail.com>
2026-03-10 22:08:44 +0530

    Update dataset_loader script

--- backend/dataset_loader.py
+++ backend/dataset_loader.py
@@ -1,8 +1,19 @@
 from datasets import load_dataset
 from configs.config import DATASET_NAME
 
+
 def load_training_dataset():
 
-    dataset = load_dataset(DATASET_NAME, split='train')
+    dataset = load_dataset(DATASET_NAME)
+
+    dataset = dataset["train"]
+
+    def format_prompt(example):
+
+        return {
+            "text": f"### Instruction:\n{example['instruction']}\n\n### Response:\n{example['response']}"
+        }
+
+    dataset = dataset.map(format_prompt)
 
     return dataset
